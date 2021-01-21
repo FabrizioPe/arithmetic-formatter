2a8762c0a9d3fe1ae30b13e4d969cdfd23936207
Fabrizio Pellegrini <fab.pellegr@gmail.com>
2021-01-21 19:00:43 +0000

    function arithmetic_arranger works fine if the input is correct (both with second argument True or False). Next step: input error detection.

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,4 +1,46 @@
-def arithmetic_arranger(problems):
+def operation(first_op, second_op, operator):
+    '''
+	Takes two operands and returns their addition
+	or subtraction according to operator
+	'''
+    if operator == '+':
+        return first_op + second_op
+    else:
+        return first_op - second_op
 
 
-    return arranged_problems
+def arithmetic_arranger(problems, include_results=False):
+
+	# each problem is converted to a list, e.g.:
+	# "23 + 456" --> [23, '+', 456]
+	problems_list = [] 
+	for problem in problems:
+		l = []
+		l = problem.split()
+		l.append(max(len(l[0]), len(l[2])))
+		l[0] = int(l[0])
+		l[2] = int(l[2])
+		problems_list.append(l)
+       
+		# building the string with arranged problems line by line
+		arranged = ''
+		# first line
+		for l in problems_list:
+			arranged += f'{l[0]:{l[3]+2}d}    '
+		arranged = arranged.rstrip() + '\n'
+		# second line
+		for l in problems_list:
+			arranged += f'{l[1]}' + f'{l[2]:{l[3]+1}d}    '
+		arranged = arranged.rstrip() + '\n'
+		# third line (dashes at the bottom)
+		for l in problems_list:
+			arranged += '-' * (l[3]+2) + '    '
+		arranged = arranged.rstrip()
+		# include results if the user asked for it
+		if include_results:
+			arranged += '\n'
+			for l in problems_list:
+				arranged += f'{operation(l[0], l[2], l[1]):{l[3]+2}d}    '		
+			arranged = arranged.rstrip()
+		
+	return arranged
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 # This entrypoint file to be used in development. Start by reading README.md
-from arithmetic_arranger import arithmetic_arranger
+from arithmetic_arranger import arithmetic_arranger, operation
 from unittest import main
 
 
-print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
+print(arithmetic_arranger(["11 + 4", "3801 - 2999", "1 + 2", "123 + 49", "1 - 9380"], True))
+# print(arithmetic_arranger("300 - 6485"))
+# print(operation(5, 7, '-'))
 
 
 # Run unit tests automatically
