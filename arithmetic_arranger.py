2c0b01eb6ac2d8c41d9b22ff031576dc1d16a3f2
Fabrizio Pellegrini <fab.pellegr@gmail.com>
2021-01-22 10:21:14 +0000

    Project finished. All tests passed.

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,7 +1,35 @@
+import re
+
+
+def input_check(problems):
+    # check if the problems are not too many
+    if len(problems) > 5:
+        return 'Error: Too many problems.'
+    # check that operators are + or -
+    pattern1 = '\w+ \+ \w+'
+    pattern2 = '\w+ \- \w+'
+    for problem in problems:
+        if (re.search(pattern1, problem) == None) and (re.search(
+                pattern2, problem) == None):
+            return "Error: Operator must be '+' or '-'."
+    # check that operands are numbers
+    for problem in problems:
+        l = problem.split()
+        if not l[0].isdigit() or not l[2].isdigit():
+            return 'Error: Numbers must only contain digits.'
+    # check that operands are not longer than 4 digits
+    for problem in problems:
+        l = problem.split()
+        if len(l[0]) > 4 or len(l[2]) > 4:
+            return 'Error: Numbers cannot be more than four digits.'
+
+    return ''
+
+
 def operation(first_op, second_op, operator):
     '''
-	Takes two operands and returns their addition
-	or subtraction according to operator
+	Takes two operands and returns their sum or
+	difference according to operator
 	'''
     if operator == '+':
         return first_op + second_op
@@ -11,36 +39,41 @@ def operation(first_op, second_op, operator):
 
 def arithmetic_arranger(problems, include_results=False):
 
-	# each problem is converted to a list, e.g.:
-	# "23 + 456" --> [23, '+', 456]
-	problems_list = [] 
-	for problem in problems:
-		l = []
-		l = problem.split()
-		l.append(max(len(l[0]), len(l[2])))
-		l[0] = int(l[0])
-		l[2] = int(l[2])
-		problems_list.append(l)
-       
-		# building the string with arranged problems line by line
-		arranged = ''
-		# first line
-		for l in problems_list:
-			arranged += f'{l[0]:{l[3]+2}d}    '
-		arranged = arranged.rstrip() + '\n'
-		# second line
-		for l in problems_list:
-			arranged += f'{l[1]}' + f'{l[2]:{l[3]+1}d}    '
-		arranged = arranged.rstrip() + '\n'
-		# third line (dashes at the bottom)
-		for l in problems_list:
-			arranged += '-' * (l[3]+2) + '    '
-		arranged = arranged.rstrip()
-		# include results if the user asked for it
-		if include_results:
-			arranged += '\n'
-			for l in problems_list:
-				arranged += f'{operation(l[0], l[2], l[1]):{l[3]+2}d}    '		
-			arranged = arranged.rstrip()
-		
-	return arranged
+    # check that input is correct
+    input_error = input_check(problems)
+    if input_error:
+        return input_error
+
+    # each problem is converted to a list, e.g.:
+    # "23 + 456" --> [23, '+', 456]
+    problems_list = []
+    for problem in problems:
+        l = []
+        l = problem.split()
+        l.append(max(len(l[0]), len(l[2])))
+        l[0] = int(l[0])
+        l[2] = int(l[2])
+        problems_list.append(l)
+
+    # building the string with arranged problems line by line
+    arranged = ''
+    # first line
+    for l in problems_list:
+        arranged += f'{l[0]:{l[3]+2}d}    '
+    arranged = arranged.rstrip() + '\n'
+    # second line
+    for l in problems_list:
+        arranged += f'{l[1]}' + f'{l[2]:{l[3]+1}d}    '
+    arranged = arranged.rstrip() + '\n'
+    # third line (dashes at the bottom)
+    for l in problems_list:
+        arranged += '-' * (l[3] + 2) + '    '
+    arranged = arranged.rstrip()
+    # fourth line: include results if the user asked for it
+    if include_results:
+        arranged += '\n'
+        for l in problems_list:
+            arranged += f'{operation(l[0], l[2], l[1]):{l[3]+2}d}    '
+        arranged = arranged.rstrip()
+
+    return arranged
--- main.py
+++ main.py
@@ -2,11 +2,7 @@
 from arithmetic_arranger import arithmetic_arranger, operation
 from unittest import main
 
-
 print(arithmetic_arranger(["11 + 4", "3801 - 2999", "1 + 2", "123 + 49", "1 - 9380"], True))
-# print(arithmetic_arranger("300 - 6485"))
-# print(operation(5, 7, '-'))
-
 
 # Run unit tests automatically
 main(module='test_module', exit=False)
